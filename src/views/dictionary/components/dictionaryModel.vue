99fdcce5e1a68f04cf116a86f50e97abd145ee5b
yi.ziqi <yi.ziqi@runyang.biz>
2023-09-06 14:56:09 +0800

    update dictionary dialog

--- src/views/dictionary/components/dictionaryModel.vue
+++ src/views/dictionary/components/dictionaryModel.vue
@@ -6,7 +6,12 @@
       width="800"
       :before-close="handleClose"
     >
-      <el-form label-position="top" label-width="120px" :model="formData">
+      <el-form
+        label-position="top"
+        label-width="120px"
+        :rules="formRules"
+        :model="formData"
+      >
         <el-row :gutter="48">
           <el-col :span="12">
             <el-form-item label="类别编码" prop="itemCode">
@@ -29,7 +34,47 @@
           </el-col>
         </el-row>
         <el-row>
-          <el-form-item label="项列表"> </el-form-item>
+          <el-form-item label="项列表">
+            <el-button
+              style="margin-bottom: 10px"
+              type="primary"
+              @click="addNewItem"
+              >新增一项</el-button
+            >
+            <el-table :data="formData.itemList" style="width: 100%">
+              <el-table-column prop="itemCode" label="项编码">
+                <template #default="scope">
+                  <el-input v-model="scope.row.itemCode"></el-input>
+                </template>
+              </el-table-column>
+              <el-table-column prop="itemName" label="项名称">
+                <template #default="scope">
+                  <el-input v-model="scope.row.itemName"></el-input>
+                </template>
+              </el-table-column>
+              <el-table-column prop="extend1" label="扩展1">
+                <template #default="scope">
+                  <el-input v-model="scope.row.extend1"></el-input>
+                </template>
+              </el-table-column>
+              <el-table-column prop="extend2" label="扩展2">
+                <template #default="scope">
+                  <el-input v-model="scope.row.extend2"></el-input>
+                </template>
+              </el-table-column>
+              <el-table-column prop="operation" label="操作">
+                <template #default="scope">
+                  <el-button
+                    type="danger"
+                    text
+                    @click="rowClickHandler('delete', scope.row, scope.$index)"
+                  >
+                    删除
+                  </el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-form-item>
         </el-row>
       </el-form>
       <template #footer>
@@ -44,6 +89,7 @@
 
 <script setup>
 import { ref } from "vue";
+import { Delete, Check, Close, Edit } from "@element-plus/icons-vue";
 const emit = defineEmits();
 const props = defineProps({
   dialogVisible: {
@@ -63,9 +109,29 @@ const formData = ref({
   itemCode: "",
   itemName: "",
   itemDescription: "",
+  itemList: [],
+});
+const formRules = ref({
+  itemCode: [{ required: true, message: "请输入类别编码", trigger: "blur" }],
+  itemName: [{ required: true, message: "请输入类别名称", trigger: "blur" }],
 });
 const submitHandler = () => {
   console.log(1);
+};
+const addNewItem = () => {
+  formData.value.itemList.push({
+    itemCode: "",
+    itemName: "",
+    extend1: "",
+    extend2: "",
+  });
+};
+const rowClickHandler = (type, row, index) => {
+  switch (type) {
+    case "delete":
+      formData.value.itemList.splice(index, 1);
+      break;
+  }
 };
 </script>
 
