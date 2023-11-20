27ffcc33dc5a97c0495c2b834f56d11e3472b735
yi.ziqi <yi.ziqi@runyang.biz>
2023-11-20 15:07:53 +0800

    update page config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 线上地址：
   https://admin.memtoys.com/
 swagger：
-  http://43.159.137.119:8084/diyadmin/doc.html
+  http://43.159.137.119:8083/diyadmin/doc.html
--- src/views/pageConfig/components/pageConfigDialog.vue
+++ src/views/pageConfig/components/pageConfigDialog.vue
@@ -6,71 +6,79 @@
       width="1000"
       :before-close="handleClose"
     >
-    <div v-loading="dialogLoading">
-      <div class="editor-container">
-        <Toolbar
-          style="border-bottom: 1px solid #ccc"
-          :editor="editorRef"
-          :defaultConfig="toolbarConfig"
-          mode="default"
-        />
-        <Editor
-          style="height: 500px; width: 100%; overflow-y: hidden"
-          v-model="formData.content"
-          :defaultConfig="editorConfig"
-          mode="default"
-          @onCreated="handleCreated"
-        />
-      </div>
-      <div class="button-container">
+      <div v-loading="dialogLoading">
+        <div class="editor-container">
+          <Toolbar
+            style="border-bottom: 1px solid #ccc"
+            :editor="editorRef"
+            :defaultConfig="toolbarConfig"
+            mode="default"
+          />
+          <Editor
+            style="height: 500px; width: 100%; overflow-y: hidden"
+            v-model="formData.content"
+            :defaultConfig="editorConfig"
+            mode="default"
+            @onCreated="handleCreated"
+          />
+        </div>
+        <div class="button-container">
           <el-button @click="handleClose">取消</el-button>
-          <el-button type="primary" @click="saveHandler" v-loading="submitLoading">保存</el-button>
+          <el-button
+            type="primary"
+            @click="saveHandler"
+            v-loading="submitLoading"
+            >保存</el-button
+          >
+        </div>
       </div>
-    </div>
-    </el-dialog> 
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
-import "@wangeditor/editor/dist/css/style.css" // 引入 css
-import { Editor, Toolbar } from "@wangeditor/editor-for-vue"
-import { computed, onBeforeUnmount, ref, shallowRef, watch } from 'vue';
-import { updateArticle, getArticleDetailById} from '@/api/article'
-import { prop } from "dom7";
+import "@wangeditor/editor/dist/css/style.css"; // 引入 css
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import { computed, onBeforeUnmount, ref, shallowRef, watch } from "vue";
+import { updateArticle, getArticleDetailById } from "@/api/article";
+import { ElMessage } from "element-plus";
+import { buildImageUrl } from "@/utils/index.js";
 
 const formData = ref({
   id: null,
   title: null,
-  content: '',
-})
+  content: "",
+});
 
 //#region wangEditor config
-const editorRef = shallowRef()
-const toolbarConfig = {}
+const editorRef = shallowRef();
+const toolbarConfig = {};
 const editorConfig = {
   placeholder: "请输入内容...",
   MENU_CONF: {
     uploadImage: {
       fieldName: "file",
-      server: "/admin/api/story/file/upload",
+      server: "/diyadmin/upload",
       headers: {
-        Authorization: localStorage.getItem('diy-admin-token')
+        Authorization: localStorage.getItem("diy-admin-token"),
       },
       customInsert(res, insertFn) {
         // 从 res 中找到 url，然后插入图片
-        insertFn(res.data.url)
-      }
-    }
-  }
-}
+        // const url = `https://admin.memtoys.com/diyadmin/image/${res.data}`;
+        const url = `https://admin.memtoys.com/diyadmin/download?fileId=${res.data}`;
+        insertFn(url);
+      },
+    },
+  },
+};
 onBeforeUnmount(() => {
-  const editor = editorRef.value
-  if (editor == null) return
-  editor.destroy()
-})
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.destroy();
+});
 const handleCreated = (editor) => {
-  editorRef.value = editor // 记录 editor 实例，重要！
-}
+  editorRef.value = editor; // 记录 editor 实例，重要！
+};
 //#endregion
 
 const props = defineProps({
@@ -81,35 +89,51 @@ const props = defineProps({
     },
   },
 });
-const dialogShow = computed(() => props.dataset.show)
-const dialogLoading = ref(false)
-watch(() => props.dataset, (data) => {
-  if (data.datas.id) {
-    dialogLoading.value = true
-    getArticleDetailById(data.datas.id).then(res => {
-      formData.value = res.data
-    }).finally(() => {
-      dialogLoading.value = false
-    })
-  }
-}, {deep: true})
+const dialogShow = computed(() => props.dataset.show);
+const dialogLoading = ref(false);
+watch(
+  () => props.dataset,
+  (data) => {
+    if (data.datas.id) {
+      dialogLoading.value = true;
+      getArticleDetailById(data.datas.id)
+        .then((res) => {
+          formData.value = res.data;
+        })
+        .finally(() => {
+          dialogLoading.value = false;
+        });
+    }
+  },
+  { deep: true }
+);
 const emit = defineEmits();
 const handleClose = () => {
+  formData.value = {
+    id: null,
+    title: null,
+    content: "",
+  };
   emit("close");
 };
-const submitLoading = ref(false)
+const submitLoading = ref(false);
 const saveHandler = () => {
-  submitLoading.value = true
+  submitLoading.value = true;
   updateArticle({
     id: props.dataset.datas.id,
     title: props.dataset.datas.title,
-    content: formData.value.content
-  }).then(res => {
-    console.log(res)
-  }).finally(() => {
-    submitLoading.value = false
+    content: formData.value.content,
   })
-}
+    .then((res) => {
+      if (res.code === 200) {
+        ElMessage.success("保存成功");
+        handleClose();
+      }
+    })
+    .finally(() => {
+      submitLoading.value = false;
+    });
+};
 </script>
 
 <style lang="less" scoped>
